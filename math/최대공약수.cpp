dd55d81c0803fcdbd7e9f5be634c733e331a06fa
auddl0756 <cjddl789123@naver.com>
2020-08-22 09:01:33 +0900

    GCD

---
+++ math/최대공약수.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+#define pii pair<int,int>
+#define ppi pair<pair<int,int>,int>
+#define sorta(a) sort(a.begin(),a.end());
+#define rsorta(a) sort(a.rbegin(),a.rend());
+#define sorta2(a,n) sort(a,a+n);
+
+#define p(a) for(int i=0;i<a.size();i++)cout<<a[i]<<" ";cout<<"\n";
+#define p2(a,n) for(int i=0;i<n;i++)cout<<a[i]<<" ";cout<<"\n";
+
+#define all(v) (v).begin(), (v).end()
+#define maxi(a,b,c) max(a,max(b,c))
+#define mini(a,b,c) min(a,min(b,c))
+#define fi first
+#define se second
+
+typedef long long ll;
+using namespace std;
+
+int GCD(int a, int b)
+{
+	int c;
+	while (b != 0)
+	{
+		c = a % b;
+		a = b;
+		b = c;
+	}
+	return a;
+}
+
+int main(){
+	ios_base::sync_with_stdio(false); cin.tie(0);
+	int a,b; cin>>a>>b;
+	int gcd = GCD(a,b);
+	cout<<gcd<<"\n";
+
+}
+
