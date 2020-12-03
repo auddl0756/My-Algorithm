83fb467c1c40f3caba23332882a3a17d60febaf8
auddl0756 <cjddl789123@naver.com>
2020-12-03 17:04:13 +0900

    분할정복 이용한 빠른 거듭제곱

---
+++ divide&conquer/빠른거듭제곱.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define all(v) v.begin(),v.end()
+#define debug(a) for(int i=0;i<a.size();i++)cout<<a[i]<<" ";cout<<"\n";
+#define pii pair<int,int>
+
+typedef long long ll;
+
+using namespace std;
+
+const int MAX=2e5+10;
+ll mod;
+
+ll solve(ll a,ll b){
+	if(b==0) return 1ll;
+
+	if(b%2){		//지수가 홀수 
+		return (solve(a,b-1)%mod)*a;		//재귀적으로 들어가서 mod먼저 안 하면 오버플로우 발생. 
+	}else{			//지수가 짝수 
+		return (solve(a,b/2)%mod)*(solve(a,b/2)%mod);
+	}
+}
+
+int main(){
+	ios_base::sync_with_stdio(false); cin.tie(0);
+	//calc a^b %mod
+	//지수를 반씩 나눠서 분할정복.
+	
+	ll a,b; cin>>a>>b>>mod;
+	
+	cout<<solve(a,b)%mod<<"\n";
+
+}
+
+
+
+
